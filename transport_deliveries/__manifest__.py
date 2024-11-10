7bad7d4ec1166530f599444e70a65271c7ca2fca
IgorSoftex <igor.softex@ukr.net>
2024-11-10 19:13:20 +0200

    Commit_transport_01 10.11.2024 19:13

---
+++ transport_deliveries/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': 'Automation of transport deliveries in Odoo',
+    'description': "Module for ordering of transport deliveries",
+    'summary': "Odoo module for automation of transport deliveries",
+    'version': '17.0.1.0.0',
+    'category': 'Customizations',
+    'license': 'LGPL-3',
+    'website': 'https://odoo.school/',
+    'author': 'Ihor Pastushenko',
+
+    'depends': [
+        'base',
+    ],
+
+    'external_dependencies': {
+        'python': [],
+    },
+
+    'data': [
+    ],
+
+    'demo': [
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+    'images': [
+    ],
+}
